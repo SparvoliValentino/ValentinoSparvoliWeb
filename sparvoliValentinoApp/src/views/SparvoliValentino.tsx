687f6b782342ab7d0fc2696fb2c86cbd291e03fa
ValentinoSparvoli <Sparvolivalentino@gmail.com>
2024-12-19 17:11:42 -0300

    tarjeta de proyecto 1

--- sparvoliValentinoApp/src/views/SparvoliValentino.tsx
+++ sparvoliValentinoApp/src/views/SparvoliValentino.tsx
@@ -1,9 +1,16 @@
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import FotoPerfil from "../assets/FotoPerfil_Valentino_Sparvoli.jpeg"
-import CybergamesLogo from "../assets/emoji-removebg-preview.png"
+// import CybergamesLogo from "../assets/emoji-removebg-preview.png"
+import nextJsLogo from "../assets/next.jsLogo.png"
+import nestJsLogo from "../assets/nestjsLogo.png"
+import mpLogo from "../assets/mpLogo.png"
+import cloudinaryLogo from "../assets/cloduinaryLogo.png"
+import swaggerLogo from "../assets/swaggerLogo.png"
+import auth0Logo from "../assets/auth0Logo.png"
+import CybergamesApp from "../assets/cybergamesApp.png"
 import { faFile } from "@fortawesome/free-solid-svg-icons";
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faFigma, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 // import CardProject from "../Components/CardProject";
 
 const SparvoliValentino = () => {
@@ -25,22 +32,28 @@ const SparvoliValentino = () => {
                 </div>
             </div>
             {/* Proyectos */}
-            <div className="w-full bg-red-50  min-h-[200px] rounded-lg">
-                <div className="relative w-[200px] h-[250px] rounded-lg">
-                    {/* Contenedor con el borde */}
-                    <div className="border-2 border-blue-700 w-full h-full rounded-lg"></div>
-
-                    {/* Imagen que sobresale */}
-                    <div className="absolute bottom-[-30px] left-1/2 transform -translate-x-1/2">
-                        <img
-                            src={CybergamesLogo}
-                            alt="Logo"
-                            className="w-[120px] h-[120px] object-cover"
-                        />
+            <div className="w-full min-h-[200px] rounded-lg flex border-2 border-blue-500">
+                <div className="w-2/3  min-h-[200px] p-3 flex flex-col">
+                    <h2 className="font-bold text-violet-500 text-[30px]">CyberGamer</h2>
+                    <p className="font-regular text-white text-[15px]">Cybergames is an app developed for Henry's final project in Fullstack Web Developer. The app consists of a video game e-commerce where the user can buy products and subscribe to a service using real money.</p>
+                    <div className="flex gap-2 mt-2">
+                        <p className="font-thin text-white text-[10px]">Development time:</p>
+                        <p className="font-thin text-white text-[10px]">4 weeks</p>
+                    </div>
+                    <div className="flex gap-4">
+                        <p className="text-white">Tecnologies used:</p>
+                        <img src={nextJsLogo} alt="Next.js" className="w-[40px] h-[40px] "/>
+                        <img src={nestJsLogo} alt="Nest.js" className="w-[60px] h-[40px]"/>
+                        <img src={mpLogo} alt="Mercado pago checkout pro" className="w-[40px] h-[40px]"/>
+                        <img src={cloudinaryLogo} alt="Cloudinary" className="w-[60px] h-[40px]"/>
+                        <FontAwesomeIcon icon={faFigma} className="text-[40px]"/>
+                        <img src={swaggerLogo} alt="Swagger" className="w-[40px] h-[40px]"/>
+                        <img src={auth0Logo} alt="Auth0" className="w-[40px] h-[40px]"/>
                     </div>
                 </div>
-
-
+                <div className="w-1/3 min-h-[200px] p-2 ">
+                    <img src={CybergamesApp} alt="" className="w-full h-full rounded-md"/>
+                </div>
 
             </div>
         </div>
